897d4139d0438d95b8f1516dabd4a09fbb5efe18
veera srinivas <srinivas_raneru2003@yahoo.com>
2020-12-02 09:42:09 +0530

    new api student

---
+++ api/controllers/StudentController.js
@@ -0,0 +1,12 @@
+/**
+ * StudentController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
